0bc2bc9a9bc9d79ffdb29c5e040ce442a43d37eb
Alex Silva <106414021+httplex@users.noreply.github.com>
2023-10-17 03:49:20 +0000

    add coments

--- leitura.c
+++ leitura.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h> //Para entrada e saída
+#include <stdlib.h> //Para funções de alocação de memória
+#include <string.h> //Para manipulação de Strings
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -9,16 +9,16 @@ int main(int argc, char *argv[]) {
     }
 
     char *nomeArquivo = argv[1];
-    FILE *arquivo = fopen(nomeArquivo, "r");
+    FILE *arquivo = fopen(nomeArquivo, "r"); //abre o arquivo para leitura, assume que o segundo arg é o nome do arquivo
 
     if (arquivo == NULL) {
-        perror("Erro ao abrir o arquivo");
+        perror("Erro ao abrir o arquivo");  //Mensagem de erro
         return 1;
     }
 
-    char linha[256];
+    char linha[256]; //definindo tamanho maximo da linha
     while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-        // Processar cada linha do arquivo CSV aqui
+        //Processar as linhas do arquivo csv
         for (int i = 0; i < strlen(linha); i++) {
             if (linha[i] == ',') {
                 linha[i] = ' '; // Substituir ';' por espaço em branco
